ac851e81e4d96b459eed7aa591080439fb278ae2
William Petit <wpetit@cadoles.com>
2016-06-28 17:20:12 +0200

    Base plugin dashboard

---
+++ plugins/DashboardPlugin/Controller/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DashboardPlugin\Controller;
+
+use Karambol\Controller\Controller;
+use Karambol\KarambolApp;
+
+class DashboardController extends Controller {
+
+  public function mount(KarambolApp $app) {
+    
+  }
+
+}
---
+++ plugins/DashboardPlugin/DashboardPagesSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DashboardPlugin;
+
+use Karambol\KarambolApp;
+use Karambol\Setting\SettingEntry;
+use Karambol\VirtualSet\ItemCountEvent;
+use Karambol\VirtualSet\ItemSearchEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Karambol\Util\AppAwareTrait;
+
+class DashboardPagesSubscriber implements EventSubscriberInterface {
+
+  public static function getSubscribedEvents() {
+    return [
+      ItemSearchEvent::NAME => 'onSearchPages',
+      ItemCountEvent::NAME => 'onCountPages'
+    ];
+  }
+
+  public function onSearchPages(ItemSearchEvent $event) {
+
+  }
+
+  public function onCountPages(ItemCountEvent $event) {
+
+  }
+
+}
