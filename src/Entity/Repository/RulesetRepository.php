2a72199c7bf74aed571b16e5db2538f289eebb74
Benjamin <developpment@gaudebenjamin.com>
2017-01-07 23:08:25 +0100

    Licence entete fichier entity

--- src/Entity/Repository/RulesetRepository.php
+++ src/Entity/Repository/RulesetRepository.php
@@ -1,9 +1,28 @@
 <?php
-
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace Karambol\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Repository pour jeu de regles
+ * @package Karambol
+ * @license AGPLv3
+ * @author William Petit
+ */
 class RulesetRepository extends EntityRepository {
 
   /**
@@ -11,6 +30,7 @@ class RulesetRepository extends EntityRepository {
    *
    * @param string $rulesetName The ruleset's name
    * @return boolean
+   * @author William Petit
    */
   public function exists($rulesetName) {
     $qb = $this->createQueryBuilder('r');
